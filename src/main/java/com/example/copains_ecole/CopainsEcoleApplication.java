3c94c49e8e02a03036690a3523d3352674fe4111
Nadia HANIFI <nadia.hanifi09@gmail.com>
2021-04-27 12:07:11 +0200

    Initial commit

---
+++ src/main/java/com/example/copains_ecole/CopainsEcoleApplication.java
@@ -0,0 +1,13 @@
+package com.example.copains_ecole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopainsEcoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopainsEcoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/copains_ecole/CopainsEcoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.copains_ecole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopainsEcoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
