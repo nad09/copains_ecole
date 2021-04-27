387fc215338df4e2ef73ab7fbe8554a7f093f80f
Corentin <cgibrat31@gmail.com>
2021-04-27 16:41:49 +0200

    ajout user Dao

--- src/main/java/com/example/copains_ecole/model/dao/UserDao.java
+++ src/main/java/com/example/copains_ecole/model/dao/UserDao.java
@@ -1,4 +1,13 @@
 package com.example.copains_ecole.model.dao;
 
-public class UserDao {
+import com.example.copains_ecole.model.UserBean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+public interface UserDao extends JpaRepository<UserBean, Long> {
+    ArrayList<UserBean> findByPseudo(String pseudo);
+    //ArrayList<UserBean> findByGroup_users(Integer group_users);
 }
