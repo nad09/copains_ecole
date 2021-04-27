3a17264bf339afe6ab680da48efe67813f882953
Nadia HANIFI <nadia.hanifi09@gmail.com>
2021-04-27 16:37:34 +0200

    add UserDao

---
+++ src/main/java/com/example/copains_ecole/model/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.example.copains_ecole.model.dao;
+
+
+import com.example.copains_ecole.model.UserBean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+public interface UserDao extends JpaRepository<UserBean, Long> {
+    ArrayList<UserBean> findByPseudo(String pseudo);
+    
+}
